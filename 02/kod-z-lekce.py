c291c2bbf313a4bb85dafdcc666d8bba7ea949b8
Andrea Waltlova <andywaltlova@gmail.com>
2024-03-19 20:45:28 +0100

    Posledni kod z lekce 2
    Signed-off-by: Andrea Waltlova <andywaltlova@gmail.com>

--- 02/kod-z-lekce.py
+++ 02/kod-z-lekce.py
@@ -54,11 +54,52 @@ vysledek = greet_user_v2(name)
 print(vysledek)
 
 
-# Nepovinne argumenty + ruzne zpusoby volani funkce s jejimi parametry - 1 otazka v kvizu
+# Nepovinne parametry + ruzne zpusoby volani funkce s jejimi parametry - 1 otazka v kvizu
+
+# Oba parametry povinne
+def mult(a, b):
+    return a * b
+
+print(mult(2, 3))
+print(mult(a=2, b=3))
+print(mult(b=2, a=3))
+print(mult(2, b=3))
+
+# Nefunguje protoze positional musi byt pred keyword parametrem
+# print(mult(a=2, 3))
+
+def mult_tri_hodnoty(a, b=5, c=10):
+    return a * b * c
+
+print(mult_tri_hodnoty(2))
+print(mult_tri_hodnoty(a=10, b=10, c=100))
+
 
 
 # Typovani - pouze zminka o tom abyste vedeli ze existuje - 1 otazka v kvizu
+# Oba parametry povinne, pozor ze python vas stejne necha zadat i jiny datovy typ ktery muze zpusobit neocekavane vysledky
+def mult(a: int, b: int=10) -> int:
+    ','.join([])
+    return a * b
 
+print(mult('3', 10))
+
+# V kombinaci s assert nize byste mohli neocekavane chovani zachytit
+# assert mult(3, 10) == 30, "Vysledek by mel byt tricet"
+# assert mult('3', 10) == 30, "Vysledek by mel byt tricet ikdyz treba zadam str misto int"
 
 # Assert - 1 otazka v kvizu
 
+def mult_rozbita(a, b):
+    return a * b+1
+
+def mult(a, b):
+    return a * b
+
+vysledek = mult(10, 10)
+assert vysledek == 100, "Funkce by mela pouze vynasobit dve cisla, vtomto pripade 10 * 10"
+
+# vysledek = mult_rozbita(10, 10)
+# assert vysledek == 100, "Funkce by mela pouze vynasobit dve cisla, vtomto pripade 10 * 10"
+
+print('Vse v poradku')
