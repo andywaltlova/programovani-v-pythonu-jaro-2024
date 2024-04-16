8bae4a425122244cdc5f2b293e6f08003682a731
Andrea Waltlova <andywaltlova@gmail.com>
2024-04-16 19:01:57 +0200

    Add kod-z-lekce for 06
    Signed-off-by: Andrea Waltlova <andywaltlova@gmail.com>

--- 06/kod-z-lekce.py
+++ 06/kod-z-lekce.py
@@ -7,26 +7,50 @@ class Employee:
         self.position = position
         self.holiday_entitlement = holiday_entitlement
 
-    def get_info(self):
+    def __str__(self):
         return f'Zaměstnanec {self.name} pracuje jako {self.position}.'
 
 class Manager(Employee):
-
-    def __init__(self, name, position, subordinates, car):
-        super().__init__(name, position)
-        self.holiday = 30
+    def __init__(self, name, position, subordinates: list, car):
+        super().__init__(name, position, 30)
         self.subordinates = subordinates
         self.car = car
 
     def __str__(self):
-        return super().__str__() + " a je to manazer"
+        return super().__str__() + " [Manazer]"
+
+class Brigadnik(Employee):
+    def __init__(self, name, position):
+        super().__init__(name, position, 0)
+
+    def __str__(self):
+        return super().__str__() + " [Brigadnik]"
+
+class Salesman(Employee):
+    def __init__(self, name, position, holiday_entitlement, car):
+        super().__init__(name, position, holiday_entitlement)
+        self.car = car
+
+    def __str__(self):
+        return super().__str__() + " [Brigadnik]"
 
 
+# Podrizeni
+karolina = Employee('Karolina', 'Prodavacka zmrzliny', 0)
+adolf = Brigadnik('Adolf', 'Ridic tramvaje')
+ruzenka = Salesman('Ruzenka', 'Prodavacka pracich prasku', 25, 'Ford')
 
+# Manager
+podrizeni = [karolina, adolf, ruzenka]
+karel = Manager('Karel', 'Prodavac kafe na hlavnim nadrazi', podrizeni, 'Skoda')
 
+for podrizeny in karel.subordinates:
+    print(isinstance(podrizeny, Employee))
+    print(podrizeny.holiday_entitlement)
 
 # Cviceni
 
 # Github - https://kodim.cz/czechitas/daweb/zaklady-gitu/uvod-do-gitu/system-git, https://github.com/
 
-# Cviceni? - jen vyzkouseni s koucem, zalozeni repoziare
+
+
